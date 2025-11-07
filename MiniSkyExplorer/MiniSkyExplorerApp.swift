46a789c8e8ad4e151d9f04459cbe65f4d85ef9af
Michela DAuria <michela.daur@gmail.com>
2025-11-07 12:46:18 +0100

    Initial Commit

---
+++ MiniSkyExplorer/MiniSkyExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniSkyExplorerApp.swift
+//  MiniSkyExplorer
+//
+//  Created by Michela D'Auria on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MiniSkyExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
